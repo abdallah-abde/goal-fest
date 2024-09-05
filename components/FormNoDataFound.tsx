cec676b8807f1397fa72f702090ad216a735c0f5
abdallah-abde <abdallah.abde@gmail.com>
2024-09-05 20:25:47 +0300

    test commit after the .gitignore fixes

--- components/FormNoDataFound.tsx
+++ components/FormNoDataFound.tsx
@@ -2,7 +2,7 @@ import { Ban } from "lucide-react";
 
 export default function FormNoDataFound({ text }: { text: string }) {
   return (
-    <div className='p-2 rounded-md w-full bg-destructive/10 text-destructive self-end text-sm flex gap-2 items-center'>
+    <div className='p-3 rounded-md w-full bg-destructive/10 text-destructive self-end text-sm flex gap-2 items-center'>
       <Ban size={20} />
       {text}
     </div>
